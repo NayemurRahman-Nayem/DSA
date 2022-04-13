42c50c03a8d4fb54a396a0ac3bedf97700624495
Nayemur Rahman Nayem <48326626+NayemurRahman-Nayem@users.noreply.github.com>
2022-04-14 03:19:17 +0600

    Update SIEVE.cpp

--- SIEVE.cpp
+++ SIEVE.cpp
@@ -1,15 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+typedef long long ll ; 
+const int mx = 1e5+123 ; 
 
-bitset<mx>vis;
-vl prime;
+
+bitset<mx>vis ; 
+vector<ll> prime ; 
 void sieve()
 {
-    prime.push_back(2);
-    for(ll i=3;i*i<=mx;i+=2)
-        if(!vis[i])
-            for(ll j=i*i;j<mx;j+=2*i)
-                vis[j]=1;
-    for(ll i=3;i<mx;i+=2)
-        if(!vis[i])
-            prime.push_back(i);
+        vis[1]=1; 
+        for(int i=3;i*i<mx;i+=2)
+        {
+                if(!vis[i])
+                {
+                        for(int j=i*i;j<mx;j+=2*i)
+                        {
+                                vis[j] = 1 ;  
+                        }
+                }
+        }
+        prime.push_back(2) ; 
+        for(int i=3;i<mx;i+=2) 
+        {
+                if(!vis[i]) prime.push_back(i) ;  
+        }
 }
 
+int main()
+{
+        sieve() ; 
+        int n ; 
+        cin >> n; 
+        cout << prime[n-1] << endl ; 
+}
