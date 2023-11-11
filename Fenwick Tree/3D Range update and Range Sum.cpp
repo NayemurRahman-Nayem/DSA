c1f20b13cb789b971c5dc597ff579792d2c91873
Nayemur Rahman Nayem <48326626+NayemurRahman-Nayem@users.noreply.github.com>
2023-11-11 15:39:41 +0600

    Create 3D Range update and Range Sum.cpp

---
+++ Fenwick Tree/3D Range update and Range Sum.cpp
@@ -0,0 +1,98 @@
+// Bismillahir Rahmanir Rahim
+
+
+#include <bits/stdc++.h>
+using namespace std ;
+typedef long long ll ;
+typedef long double ld ;
+typedef unsigned long long ull ;
+typedef vector<int> vi ;
+typedef vector<ll> vl ;
+typedef vector<vi> vvi ;
+typedef vector<vl> vvl ;
+using u64 = unsigned long long ; 
+typedef pair<int, int> pii ;
+typedef pair<ll, ll> pll ;
+typedef pair<float, float> pff ;
+typedef pair<double, double> pdd ;
+typedef pair<ll, ll> pll ;
+#define pb push_back
+#define el << '\n' ;
+#define all(a) (a).begin(),(a).end()
+#define allr(a) (a).begin(),(a).end()
+#define F first
+#define S second
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds ;
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+#define pb push_back
+#define PI 3.14159265358979323
+#define file() freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout)
+#define fastIO	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0) ;
+ll bin_expo(ll base, ll pow, ll m) {
+    if (pow == 0) return 1;ll ret = bin_expo(base, pow / 2, m) % m ;
+    if (pow & 1) return (ret % m * ret % m * base % m) % m;else return (ret % m * ret % m) % m ; }
+ll mod_add(ll a, ll b, ll m) { return ((a % m) + (b % m)) % m ; }
+ll mod_sub(ll a, ll b, ll m) { return (a-b+m)%m ; }
+ll mod_mul(ll a, ll b, ll m) { return ((a % m) * (b % m)) % m ; }
+ll mod_div(ll a, ll b, ll m) { return ((a % m) * bin_expo(b, m - 2, m)) % m ; }
+ll aModB(ll a , ll b) { if(a<0 ) { return ((a+(abs(a)/b)*b)+b)%b ; } else { return a%b; } }
+ll lcm(ll a, ll b) { return (a * b) / __gcd(a, b) ; }
+int dx[] = {0, 0, +1, -1, +1, -1, -1, +1} ;
+int dy[] = {+1, -1, 0, 0, +1, -1, +1, -1} ;
+const ll infLL = 4e18 + 123 ;
+const ll inf = 1e12 + 7 ;
+const ll mod = 1e9 + 7 ;
+const ll mod2 = 998244353 ;
+const ll mx = 1e2 + 10 ;
+ll LOG = 21 ; 
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+ll fen[mx][mx][mx] , n , q ; 
+void update(ll x , ll y , ll z ,  ll add) {
+    while(x<=n+1) {
+        ll y1 = y ; 
+        while(y1<=n+1) {
+            ll z1 = z ; 
+            while(z1<=n+1) {
+                fen[x][y1][z1] += add ; 
+                z1 = z1 + (z1 & (-z1)) ; 
+            }
+            y1 = y1 + (y1 & (-y1)) ; 
+        }
+        x = x + (x & (-x)) ; 
+    }
+}
+ll sum(ll x, ll y , ll z)  {
+    ll s = 0 ; 
+    while(x>0) {
+        ll y1 = y ; 
+        while(y1>0) {
+            ll z1 = z ;
+            while(z1>0) {
+                s+=fen[x][y1][z1] ; 
+                z1 = z1 - (z1 & (-z1)) ; 
+            }  
+            y1 = y1 - (y1 & (-y1)) ; 
+        }
+        x = x - (x & (-x)) ; 
+    }
+    return s ; 
+}
+void solution(int cs) {
+    
+}             
+int main() { 
+    fastIO ;
+    #ifndef ONLINE_JUDGE
+    file() ;
+    #endif
+    int t = 0 ; 
+    int cs = 1 ;
+    //cin >> cs ;  
+    for(ll i=1;i<=cs;i++) solution(i) ; 
+    return 0 ;
+}
